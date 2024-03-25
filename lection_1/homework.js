b81aff6ea36457b41116d4b0df95e34bf5f1482a
katya-sav <esava1105@gmail.com>
2024-03-25 09:08:58 +0300

    add new

--- lection_1/homework.js
+++ lection_1/homework.js
@@ -1,56 +1,55 @@
-
 // Неявное преобразование типов
-// console.log(NaN + 1 + null + 1);
-// console.log(NaN + 1 + null + 1 + "");
-// console.log("" + NaN + 1 + null + 1 + "");
-// console.log("number" + 15 + 3)
-// console.log(true + false)
-// console.log([] + null + 1)
-// console.log("foo" + + "bar")
-// console.log(!!"false" == !!"true")
-// console.log(null == '')
-// console.log([] == ![])
+console.log(NaN + 1 + null + 1); // NaN
+console.log(NaN + 1 + null + 1 + ""); // "NaN"
+console.log("" + NaN + 1 + null + 1 + ""); // "NaN1null1"
+console.log("number" + 15 + 3); // "number153"
+console.log(true + false); // 1
+console.log([] + null + 1); // "null1"
+console.log("foo" + +"bar"); // "fooNaN"
+console.log(!!"false" == !!"true"); // true
+console.log(null == ""); // false
+console.log([] == ![]); // true
 
 // Логические операторы
-// console.log("a" && "b" && "c")
-// console.log("a" || "b" || "c")
-// console.log("a" && "" && "c")
-// console.log("a" && "" || "c")
-// console.log("" ?? "a")
-// console.log(null ?? "a")
+console.log("a" && "b" && "c"); // "c"
+console.log("a" || "b" || "c"); // "a"
+console.log("a" && "" && "c"); // ""
+console.log(("a" && "") || "c"); // "c"
+console.log("" ?? "a"); // ""
+console.log(null ?? "a"); // "a"
 
 // Всплытие
 
-// var a = 1;
-//
-// function foo() {
-//     console.log(a);
-// }
-//
-// function bar() {
-//     var a = 2;
-//     foo();
-// }
-//
-// bar(); // ?
+var a = 1;
+
+function foo() {
+  console.log(a);
+}
+
+function bar() {
+  var a = 2;
+  foo();
+}
+
+bar(); // 1
 ////////////////////
 
-// function foo(a) {
-//     if (a > 0) {
-//         let a = a + 10
-//         return a
-//     }
-//     return a
-// }
-// console.log(foo(15)) //?
+function foo(a) {
+  if (a > 0) {
+    let a = a + 10;
+    return a;
+  }
+  return a;
+}
+console.log(foo(15)); // ReferenceError
 ////////////////////
 
-// function giveMeX(showX) {
-//     if(showX) {
-//         let x = 5;
-//     }
-//     return x;
-// }
-//
-// console.log(giveMeX(false)); // ?
-// console.log(giveMeX(true)); // ?
+function giveMeX(showX) {
+  if (showX) {
+    let x = 5;
+  }
+  return x;
+}
+
+console.log(giveMeX(false)); // Error
+console.log(giveMeX(true)); // Error
