356bf019a0c90a45a4b031126d91576ef8975d5f
katya-sav <esava1105@gmail.com>
2024-04-04 12:09:54 +0300

    done homework lection5

---
+++ lection_5/homework.js
@@ -0,0 +1,107 @@
+//1.
+// const a = {b: 1},
+//     c = Object.create(a);
+//
+// console.log(c.b); // 1
+// delete c.b;
+// console.log(c.b); // 1
+// delete a.b;
+// console.log(c.b); // undefined
+// a.z = 2;
+// console.log(c.z); // 2
+// c.z = 3;
+// console.log(a.z); // 2
+
+// 2.
+
+// const promise = new Promise(() => {
+// })
+// promise.prototype === Promise.__proto__ // false
+//
+// const obj = {}
+// obj.__proto__ === Object.prototype // true
+//
+// new Array([]).__proto__ === Array.prototype // true
+//
+// function Fn1 () {}
+// function Fn2 () {}
+// Fn1.constructor === Fn2.constructor // true
+//
+// Fn1.prototype === Fn2.prototype // false
+//3.
+
+// У вас есть два конструктора, Animal и Bird.
+// Каждый объект типа Bird должен наследовать метод speak от Animal.
+// Однако, Bird также должен иметь свой собственный метод fly.
+
+// Создайте конструктор Animal, который принимает параметр name и устанавливает его как свойство объекта.
+// Добавьте метод speak в Animal, который выводит в консоль звук, издаваемый животным (например, "Some generic sound").
+// Создайте конструктор Bird, который принимает параметр name и вызывает конструктор Animal с тем же именем.
+// Добавьте метод fly в Bird, который выводит в консоль сообщение о том, что птица летит (например, "Flying high!").
+// Создайте объекты animal и bird с использованием соответствующих конструкторов и вызовите их методы speak и fly.
+// Решите задачу, используя прототипное наследование, чтобы Bird наследовал от Animal.
+
+// Должно быть такое поведение:
+// const animal = new Animal("Дженни");
+// const bird = new Bird("Воробей");
+//
+// animal.speak(); // "Some generic sound"
+// bird.speak();   // "Some generic sound"
+// bird.fly();     // "Flying high!"
+
+// Решение 1
+// С помощью конструкторов
+function Animal(name) {
+  this.name = name;
+
+  this.speak = function () {
+    console.log("Some generic sound");
+  };
+}
+
+function Bird(name) {
+  Animal.call(this, name); // применяем конструктор Animal
+
+  this.fly = function () {
+    console.log("Flying high!");
+  };
+}
+
+Bird.prototype = Object.create(Animal.prototype); // наследование прототипа от Animal, но при этом созданный объект указывает на конструктор Animal
+Bird.prototype.constructor = Bird; // установка конструктора Bird
+
+const animal = new Animal("Дженни");
+const bird = new Bird("Воробей");
+
+animal.speak(); // "Some generic sound"
+bird.speak(); // "Some generic sound"
+bird.fly(); // "Flying high!"
+
+// Решение 2
+// С помощью классов
+class Animal {
+  constructor(name) {
+    this.name = name;
+  }
+
+  speak() {
+    console.log("Some generic sound");
+  }
+}
+
+class Bird extends Animal {
+  constructor(name) {
+    super(name);
+  }
+
+  fly() {
+    console.log("Flying high!");
+  }
+}
+
+const animal1 = new Animal("Дженни");
+const bird1 = new Bird("Воробей");
+
+animal1.speak(); // "Some generic sound"
+bird1.speak(); // "Some generic sound"
+bird1.fly(); // "Flying high!"
